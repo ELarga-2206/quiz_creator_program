8f1fd49aa4f4cdf343731773f183c0b7e949f96a
Elarga-2206 <earl.larga22@gmail.com>
2025-04-09 12:10:19 +0800

    Creation of pseudo code—this will help me realize and interceptgaps and potential problems early on. also for guidance for structuring the program in a logical/ organized way

--- quiz_generator_main.py
+++ quiz_generator_main.py
@@ -1,16 +1,30 @@
-# so based on the instruction, we need to create a program that will ask the user:
+#so how do we ask the
+	#question
+	#the 4 choices
+	# and its answer
+	#for some reason it has to save itself to a text file 
+	#ask for a new question (Y/N)
 
-    # the questions they want
-    # the 4 choices its question have
-    # and the correct answer
-    # it should also ask if they want another question (so a while loop for sure.)
-    # inputs should also be written in a text file.
-    # so we'll have to think through the sequence carefully, make sure everything is fit and doesnt interfere with other code blocks.
+#for asking the questions → while loop
 
-# so its basically, a quiz maker.
+#while true:
+	# user=input(“your question”)
+	# ask for a key to stop the loop
+	# say thanks for using the program
+	# break
 
-# we'll also try to pull of creative stunts, im planning to import pygame, tkinter, or any 
-# library that will make it look very pleasing to the eyes. I think we'll stick to certain creative theme if my ability permits.
+#The 4 questions:
 
-# a pseudocode will follow, regarding the execution of this program. 
-    # hopefully the sequence wont cause problems and clash with each other.
+	#print(“what choices do you want”)
+	# Questions = input [ “A: B: C: D: ]
+	#break
+
+#The correct answer:
+ 	 #print (“what is the correct answer? A/B/C/D”)
+	# we need to figure out a code that will check if the answer is correct*
+	# also an error catcher in case the user inputs unknown letters
+
+# still not sure about data to text file, we have to record every input and be able to access it in a txt file. need research for this  
+
+# also about the tkinter library, still figuring out whether we can put music/sound effects, backgrounds, or any design/interactive
+# buttons. 
