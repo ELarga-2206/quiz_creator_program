5c71bc988d4b41b2bdc75128afb834a38204118a
Elarga-2206 <earl.larga22@gmail.com>
2025-04-09 11:23:04 +0800

    first python file--Brainstorming and understanding the program requirements

---
+++ quiz_generator_main.py
@@ -0,0 +1,16 @@
+# so based on the instruction, we need to create a program that will ask the user:
+
+    # the questions they want
+    # the 4 choices its question have
+    # and the correct answer
+    # it should also ask if they want another question (so a while loop for sure.)
+    # inputs should also be written in a text file.
+    # so we'll have to think through the sequence carefully, make sure everything is fit and doesnt interfere with other code blocks.
+
+# so its basically, a quiz maker.
+
+# we'll also try to pull of creative stunts, im planning to import pygame, tkinter, or any 
+# library that will make it look very pleasing to the eyes. I think we'll stick to certain creative theme if my ability permits.
+
+# a pseudocode will follow, regarding the execution of this program. 
+    # hopefully the sequence wont cause problems and clash with each other.
