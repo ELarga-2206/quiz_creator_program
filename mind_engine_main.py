c3a7721f0aeb672423dbd83d3197ac08fe8bc37d
ELarga-2206 <earl.larga22@gmail.com>
2025-05-03 22:55:44 +0800

    created a new python file -- brainstorming phase

---
+++ mind_engine_main.py
@@ -0,0 +1,20 @@
+# lets brainstorm this
+
+# our goal is a quiz reader that will read the file created by the quiz creator
+
+# so its main features are:
+# individually asking questions
+# individually asking answers
+# checking how many the user got corect or instant correction
+# recording how many questions are correct
+# we also need to explore tkinter
+
+# what we need to know so far is:
+
+# how to program a file reader
+# convert it to a quiz
+# use GUI for creativity
+
+# the aforementioned features would be studied later
+
+
