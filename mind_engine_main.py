cd2380c60878fd7e4454c576f89265d17654c2d9
ELarga-2206 <earl.larga22@gmail.com>
2025-05-04 23:27:14 +0800

    Imported necessary libraries, Added the load_quiz function – where it will: load the file, open the file and read the file, all while removing its spaces (so we can get words only)

--- mind_engine_main.py
+++ mind_engine_main.py
@@ -5,10 +5,17 @@
             # open the file "r"
                 # then file.read
 
+# use for loop
+        # convert the borders in the txt file into 'blocks'
+        # strip unecessary spaces to get the pure words only
+        # append to a list
+        # then we can call that list whenever
+
+
 import tkinter as tk
 from tkinter import messagebox # for info display
 import random 
 
 def load_quiz(filename="quiz_questions.txt"):
-    with open(filename=quiz_question.txt):
         with open(filename, "r") as file:
+                content = file.read().strip() # strip the spaces
